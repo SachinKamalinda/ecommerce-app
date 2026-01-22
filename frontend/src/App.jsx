33b20b4e88b96aea732c9a991a59ead4b15ed019
Sachiya-coder <sachinkamalinda@gmail.com>
2026-01-22 21:28:37 +0530

    Add Navbar component and integrate it into the App layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import { Routes,Route } from 'react-router-dom'
 import Home from './pages/Home.jsx'
+import Navbar from './components/Navbar.jsx'
+import Collection from "./pages/Collection";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Product from "./pages/Product";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import PlaceOrder from "./pages/PlaceOrder";
+import Orders from "./pages/Orders";
 
 const App = () => {
   return (
     <div className= 'px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
-
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/collection' element={<Collection />} />
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {assets} from '../assets/assets'
+
+const Navbar = () => {
+  return (
+    <div className='flex items-center justify-between py-5 font-medium'>
+
+        <img src={assets.logo} className='w-36' alt="Logo" />
+      
+        <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
+            
+        </ul>    
+    </div>
+  )
+}
+
+export default Navbar
