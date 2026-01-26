b57244e00d630ffb54d72165c47810a9a850975d
Sachiya-coder <sachinkamalinda@gmail.com>
2026-01-26 23:08:52 +0530

    Add Hero component with image and text sections for showcasing latest arrivals

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Hero = () => {
   return (
@@ -18,6 +19,8 @@ const Hero = () => {
             
         </div>
       </div>
+      {/* Hero Right Section */}
+      <img className='w-full sm:w-1/2' src={assets.hero_img} alt="" />
     </div>
   )
 }
